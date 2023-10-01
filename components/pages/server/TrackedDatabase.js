fdcd491a109e08a9ac867dea597d314faf03941c
Finbar <finbar@finbar.xyz>
2023-10-01 12:37:21 -0400

    editing database retention period and backup frequency works

--- components/pages/server/TrackedDatabase.js
+++ components/pages/server/TrackedDatabase.js
@@ -1,18 +1,19 @@
-import { useState } from 'react';
-import { data } from 'autoprefixer';
+import { useState } from "react";
+import { data } from "autoprefixer";
 
-export default function TrackedDatabase ({ database, index, setUntracked, setTracked, untracked, tracked }) {
+export default function TrackedDatabase({ database, index, setUntracked, setTracked, untracked, tracked }) {
   const [editing, setEditing] = useState(false);
   const [retention, setRetention] = useState(database.retentionPeriod);
   const [retentionUnit, setRetentionUnit] = useState(database.retentionPeriodUnit);
   const [frequency, setFrequency] = useState(database.backupFrequency);
   const [frequencyUnit, setFrequencyUnit] = useState(database.backupFrequencyUnit);
+  const [error, setError] = useState(null);
 
   const unTrack = () => {
     fetch(`/api/servers/${database.serverId}`, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({
-        op: 'track',
+        op: "track",
         data: {
           tracked: false,
           databaseName: database.name
@@ -24,9 +25,45 @@ export default function TrackedDatabase ({ database, index, setUntracked, setTra
         if (!data.error) {
           setUntracked([...untracked, database]);
           setTracked(tracked.filter(db => db.name !== database.name));
-        }
+        } else
+          setError(data.error);
       });
-  }
+  };
+
+  const save = () => {
+    fetch(`/api/servers/${database.serverId}`, {
+      method: "POST",
+      body: JSON.stringify({
+        op: "patch",
+        data: {
+          retentionPeriod: retention,
+          retentionPeriodUnit: retentionUnit,
+          backupFrequency: frequency,
+          backupFrequencyUnit: frequencyUnit,
+          databaseName: database.name
+        }
+      })
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data.error) {
+          setTracked(tracked.map(db => {
+            if (db.name === database.name) {
+              return {
+                ...db,
+                retentionPeriod: retention,
+                retentionPeriodUnit: retentionUnit,
+                backupFrequency: frequency,
+                backupFrequencyUnit: frequencyUnit
+              };
+            }
+            return db;
+          }));
+          setEditing(false);
+        } else
+          setError(data.error);
+      });
+  };
 
   return (
     <div className="flex flex-col w-full bg-white rounded-lg p-2 px-5 shadow-sm mt-2" key={index}>
@@ -36,31 +73,31 @@ export default function TrackedDatabase ({ database, index, setUntracked, setTra
       <p className="text-sm text-gray-500 flex flex-row gap-x-1 items-center">Retention Period: {editing
         ? (
           <div className="flex flex-row gap-x-2">
-            <input type="number" className="border border-gray-300 rounded-md px-2 py-1 text-sm text-gray-500 focus:outline-none focus:ring-[1px] focus:ring-blue-500 focus:border-transparent" value={retention} onChange={e => setRetention(e.target.value)}/>
+            <input type="number" className="border border-gray-300 rounded-md px-2 py-1 text-sm text-gray-500 focus:outline-none focus:ring-[1px] focus:ring-blue-500 focus:border-transparent" value={retention} onChange={e => setRetention(parseInt(e.target.value))} />
             <select className="border border-gray-300 rounded-md px-2 py-1 text-sm text-gray-500 focus:outline-none focus:ring-[1px] focus:ring-blue-500 focus:border-transparent" value={retentionUnit} onChange={e => setRetentionUnit(e.target.value)}>
-              <option value="hour">Hour{retention != 1 && 's'}</option>
-              <option value="day">Day{retention != 1 && 's'}</option>
-              <option value="week">Week{retention != 1 && 's'}</option>
-              <option value="month">Month{retention != 1 && 's'}</option>
+              <option value="hour">Hour{retention != 1 && "s"}</option>
+              <option value="day">Day{retention != 1 && "s"}</option>
+              <option value="week">Week{retention != 1 && "s"}</option>
+              <option value="month">Month{retention != 1 && "s"}</option>
             </select>
           </div>
         )
-        : <span className="font-semibold">for {retention} {retentionUnit}{retention != 1 && 's'}</span>
+        : <span className="font-semibold">for {retention} {retentionUnit}{retention != 1 && "s"}</span>
       }</p>
       <p className="text-sm text-gray-500 flex flex-row gap-x-1 items-center">Backup Frequency: {editing
         ? (
           <div className="flex flex-row gap-x-2 mt-2">
-            <input type="number" className="border border-gray-300 rounded-md px-2 py-1 text-sm text-gray-500 focus:outline-none focus:ring-[1px] focus:ring-blue-500 focus:border-transparent" value={frequency} onChange={e => setFrequency(e.target.value)}/>
+            <input type="number" className="border border-gray-300 rounded-md px-2 py-1 text-sm text-gray-500 focus:outline-none focus:ring-[1px] focus:ring-blue-500 focus:border-transparent" value={frequency} onChange={e => setFrequency(parseInt(e.target.value))} />
             <select className="border border-gray-300 rounded-md px-2 py-1 text-sm text-gray-500 focus:outline-none focus:ring-[1px] focus:ring-blue-500 focus:border-transparent" value={frequencyUnit} onChange={e => setFrequencyUnit(e.target.value)}>
-              <option value="hour">Hour{frequency != 1 && 's'}</option>
-              <option value="day">Day{frequency != 1 && 's'}</option>
-              <option value="week">Week{frequency != 1 && 's'}</option>
-              <option value="month">Month{frequency != 1 && 's'}</option>
+              <option value="hour">Hour{frequency != 1 && "s"}</option>
+              <option value="day">Day{frequency != 1 && "s"}</option>
+              <option value="week">Week{frequency != 1 && "s"}</option>
+              <option value="month">Month{frequency != 1 && "s"}</option>
             </select>
           </div>
         )
         :
-        <span className="font-semibold">every {frequency != 1 && frequency} {frequencyUnit}{frequency != 1 && 's'}</span>
+        <span className="font-semibold">every {frequency != 1 && frequency} {frequencyUnit}{frequency != 1 && "s"}</span>
       }</p>
       <p className="text-sm text-gray-500">Last backup: <span className="font-semibold">1 hour ago</span></p>
       <p className="text-sm text-gray-500">Current Status: <span className="font-semibold text-green-500">Backup Completed</span>
@@ -75,10 +112,11 @@ export default function TrackedDatabase ({ database, index, setUntracked, setTra
         <p className="text-gray-200 select-none">|</p>
         <p className="text-gray-500 hover:underline cursor-pointer">View Backups</p>
         <p className="text-gray-200 select-none">|</p>
-        <p className="text-gray-500 hover:underline cursor-pointer" onClick={() => setEditing(!editing)}>{editing ? 'Save' : 'Edit'}</p>
+        <p className="text-gray-500 hover:underline cursor-pointer" onClick={() => editing ? save() : setEditing(!editing)}>{editing ? "Save" : "Edit"}</p>
         <p className="text-gray-200 select-none">|</p>
         <p className="text-red-500 hover:underline cursor-pointer" onClick={unTrack}>Untrack</p>
       </div>
+      {error && <p className="text-red-500 text-sm">{error}</p>}
     </div>
-  )
+  );
 }
--- pages/api/servers/[id]/index.js
+++ pages/api/servers/[id]/index.js
@@ -1,11 +1,11 @@
-import { Server, Database } from '@/lib/db';
-import { getSession } from 'next-auth/react';
+import { Server, Database } from "@/lib/db";
+import { getSession } from "next-auth/react";
 
 export default async (req, res) => {
   const session = await getSession({ req });
 
   if (!session)
-    return res.status(401).json({ error: 'Unauthorized' });
+    return res.status(401).json({ error: "Unauthorized" });
 
   let server = await Server.findOne({
     where: {
@@ -13,11 +13,11 @@ export default async (req, res) => {
     }
   });
 
-  if(!server)
-    return res.status(404).json({ error: 'Server not found' });
+  if (!server)
+    return res.status(404).json({ error: "Server not found" });
 
-  switch(req.method) {
-    case 'GET': {
+  switch (req.method) {
+    case "GET": {
       const databases = await Database.findAll({
         where: {
           serverId: server.id
@@ -26,18 +26,18 @@ export default async (req, res) => {
 
       return res.status(200).json({ server, databases });
     }
-    case 'POST': {
+    case "POST": {
       const { op, data } = JSON.parse(req.body);
 
-      switch(op) {
-        case 'track': {
+      switch (op) {
+        case "track": {
           const { tracked, databaseName } = data;
 
-          if (typeof tracked !== 'boolean')
-            return res.status(400).json({ error: 'Invalid track value' });
+          if (typeof tracked !== "boolean")
+            return res.status(400).json({ error: "Invalid track value" });
 
-          if (!databaseName || typeof databaseName !== 'string')
-            return res.status(400).json({ error: 'Invalid databaseName value' });
+          if (!databaseName || typeof databaseName !== "string")
+            return res.status(400).json({ error: "Invalid databaseName value" });
 
           const database = await Database.findOne({
             where: {
@@ -47,10 +47,10 @@ export default async (req, res) => {
           });
 
           if (!database)
-            return res.status(404).json({ error: 'Database not found' });
+            return res.status(404).json({ error: "Database not found" });
 
           if (database.serverId !== server.id)
-            return res.status(400).json({ error: 'Database does not belong to this server' });
+            return res.status(400).json({ error: "Database does not belong to this server" });
 
           const updatedDatabase = await database.update({
             tracked
@@ -58,13 +58,50 @@ export default async (req, res) => {
 
           return res.status(200).json(updatedDatabase);
         }
+        case "patch": {
+          const { databaseName, retentionPeriod, retentionPeriodUnit, backupFrequency, backupFrequencyUnit } = data;
+
+          if (typeof retentionPeriod !== "number" || retentionPeriod < 0)
+            return res.status(400).json({ error: "Invalid retentionPeriod value" });
+
+          if (typeof retentionPeriodUnit !== "string" || !["second", "minute", "hour", "day", "week", "month", "year"].includes(retentionPeriodUnit))
+            return res.status(400).json({ error: "Invalid retentionPeriodUnit value" });
+
+          if (typeof backupFrequency !== "number" || backupFrequency < 0)
+            return res.status(400).json({ error: "Invalid backupFrequency value" });
+
+          if (typeof backupFrequencyUnit !== "string" || !["second", "minute", "hour", "day", "week", "month", "year"].includes(backupFrequencyUnit))
+            return res.status(400).json({ error: "Invalid backupFrequencyUnit value" });
+
+          const database = await Database.findOne({
+            where: {
+              serverId: server.id,
+              name: databaseName
+            }
+          });
+
+          if (!database)
+            return res.status(404).json({ error: "Database not found" });
+
+          if (database.serverId !== server.id)
+            return res.status(400).json({ error: "Database does not belong to this server" });
+
+          const updatedDatabase = await database.update({
+            retentionPeriod,
+            retentionPeriodUnit,
+            backupFrequency,
+            backupFrequencyUnit
+          });
+
+          return res.status(200).json(updatedDatabase);
+        }
         default: {
-          return res.status(400).json({ error: 'Invalid op value' });
+          return res.status(400).json({ error: "Invalid op value" });
         }
       }
     }
     default: {
-      return res.status(405).json({ error: 'Method not allowed' });
+      return res.status(405).json({ error: "Method not allowed" });
     }
   }
 }
