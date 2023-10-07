644ba63cadd8591a5ebfaa4ee55c389e5834c2ab
Finbar <finbar@finbar.xyz>
2023-10-06 20:29:42 -0400

    update env template

---
+++ server.js
@@ -0,0 +1,32 @@
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const dev = process.env.STAGE !== 'production'
+const hostname = process.env.HOSTNAME || 'localhost'
+const port = parseInt(process.env.PORT, 10) || 3000
+
+const app = next({ dev, hostname, port })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true)
+      const { pathname, query } = parsedUrl
+
+      await handle(req, res, parsedUrl)
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err)
+      res.statusCode = 500
+      res.end('internal server error')
+    }
+  })
+  .once('error', (err) => {
+    console.error(err)
+    process.exit(1)
+  })
+  .listen(port, () => {
+    console.log(`> Ready on http://${hostname}:${port}`)
+  })
+})
