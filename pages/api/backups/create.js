3a3a0ab8d034c4314255029d67ba4ed7493100f8
Finbar <finbar@finbar.xyz>
2023-10-10 15:53:21 -0400

    copying files from remote server to local directory works

--- pages/api/backups/create.js
+++ pages/api/backups/create.js
@@ -1,5 +1,6 @@
 import { sshConnectionManager } from "@/lib/utils/SSHConnectionManager";
 import { Database, Server } from "@/lib/db";
+import * as fs from 'fs';
 
 export default async (req, res) => {
   const { serverId, databaseName } = req.body;
@@ -39,19 +40,65 @@ export default async (req, res) => {
 
   const { client } = cachedServer;
 
-  client.exec(`mysqldump -u ${DBServer.mysqlUsername} -p${DBServer.mysqlPassword} ${databaseName} > ${process.env.REMOTE_CHECKOUT_DIRECTORY}/${databaseName}.sql`, (err, stream) => {
-    if (err)
-      return res.status(400).json({ error: err });
+  await new Promise((resolve) => {
+    client.exec(`mkdir -p ${process.env.REMOTE_CHECKOUT_DIRECTORY}`, (err, stream) => {
+      if (err)
+        return res.status(400).json({ error: err });
 
-    stream.on('close', (code, signal) => {
-      if (code === 0)
-        return res.status(200).json({ success: true });
+      stream.on('close', (code, signal) => {
+        if (code === 0)
+          return resolve();
 
-      return res.status(400).json({ error: `Process exited with code ${code}` });
-    }).on('data', (data) => {
-      console.log(data.toString());
-    }).stderr.on('data', (data) => {
-      console.log(data.toString());
+        return res.status(400).json({ error: `Process exited with code ${code}` });
+      }).on('data', (data) => {
+        console.log(data.toString());
+      }).stderr.on('data', (data) => {
+        console.log(data.toString());
+      });
     });
   })
+
+  await new Promise((resolve) => {
+    client.exec(`mysqldump -u ${DBServer.mysqlUsername} -p${DBServer.mysqlPassword} ${databaseName} > ${process.env.REMOTE_CHECKOUT_DIRECTORY}/${databaseName}.sql`, (err, stream) => {
+      if (err)
+        return res.status(400).json({ error: err });
+
+      stream.on('close', (code, signal) => {
+        if (code === 0)
+          return resolve();
+
+        return res.status(400).json({ error: `Process exited with code ${code}` });
+      }).on('data', (data) => {
+        console.log(data.toString());
+      }).stderr.on('data', (data) => {
+        console.log(data.toString());
+      });
+    })
+  })
+
+  let response = '';
+  await new Promise((resolve) => {
+    client.exec(`cat ${process.env.REMOTE_CHECKOUT_DIRECTORY}/${databaseName}.sql`, (err, stream) => {
+      if (err)
+        return res.status(400).json({ error: err });
+
+      stream.on('close', (code, signal) => {
+        if (code === 0)
+          return resolve();
+
+        return res.status(400).json({ error: `Process exited with code ${code}` });
+      }).on('data', (data) => {
+        response += data.toString();
+      }).stderr.on('data', (data) => {
+        console.log(data.toString());
+      });
+    })
+  })
+
+  if(!fs.existsSync(process.env.LOCAL_STORAGE_DIRECTORY))
+    fs.mkdirSync(process.env.LOCAL_STORAGE_DIRECTORY);
+
+  fs.writeFileSync(`${process.env.LOCAL_STORAGE_DIRECTORY}/${databaseName}.sql`, response);
+
+  return res.status(200).json({ success: true });
 }
