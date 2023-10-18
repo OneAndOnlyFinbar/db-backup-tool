b07cb4dec309a50024293a59267c9607f32909e9
Finbar <finbar@finbar.xyz>
2023-10-18 18:51:17 -0400

    add file size

--- components/pages/backups/Backup.js
+++ components/pages/backups/Backup.js
@@ -1,33 +1,4 @@
-const formatDurationRelative = (unix, relativeOnly = false) => {
-  const date = new Date(parseInt(unix));
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-
-  let relativeTimestamp = "Just now";
-  const diff = new Date() - date;
-  const seconds = Math.floor(diff / 1000);
-  const minutes = Math.floor(seconds / 60);
-  const hours = Math.floor(minutes / 60);
-  const days = Math.floor(hours / 24);
-  const months = Math.floor(days / 30);
-  const years = Math.floor(months / 12);
-
-  if (years > 0)
-    relativeTimestamp = `${years} year${years > 1 ? "s" : ""} ago`;
-  else if (months > 0)
-    relativeTimestamp = `${months} month${months > 1 ? "s" : ""} ago`;
-  else if (days > 0)
-    relativeTimestamp = `${days} day${days > 1 ? "s" : ""} ago`;
-  else if (hours > 0)
-    relativeTimestamp = `${hours} hour${hours > 1 ? "s" : ""} ago`;
-  else if (minutes > 0)
-    relativeTimestamp = `${minutes} minute${minutes > 1 ? "s" : ""} ago`;
-  else if (seconds > 0)
-    relativeTimestamp = `${seconds} second${seconds > 1 ? "s" : ""} ago`;
-
-  return relativeOnly ? relativeTimestamp : `${month}/${day}/${year} (${relativeTimestamp})`;
-}
+import { formatDurationRelative, bytesToSize } from "lib/utils/Functions";
 
 export default function Backup({ data }) {
   return (
@@ -35,7 +6,7 @@ export default function Backup({ data }) {
       <h1 className="text-xl">{new Date(data.date).toLocaleDateString()}</h1>
 
       <p className="text-gray-500 text-sm">{formatDurationRelative(new Date(data.date).getTime(), true)}</p>
-      <p className="text-gray-500 text-sm">Backup size: {data.size}</p>
+      <p className="text-gray-500 text-sm">Backup size: {bytesToSize(data.size)}</p>
       <p className={`text-sm text-gray-500`}>Status: <span className={`${data.status ? "text-green-500" : "text-red-500"} font-semibold`}>{data.status ? "Success" : "Failed"}</span></p>
 
       <div className="h-[1px] bg-gray-200 my-2"></div>
