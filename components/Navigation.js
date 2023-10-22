5cb7b6a67d52a4e5ff15c945294ca765b938d55a
Finbar <finbar@finbar.xyz>
2023-10-21 21:02:48 -0400

    update navbar

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,6 +1,6 @@
 import { useSession } from 'next-auth/react';
 import { useState, useEffect } from 'react';
-import { FaBars } from 'react-icons/fa';
+import { FaBars, FaServer, FaUserCircle, FaLock } from 'react-icons/fa';
 import Link from 'next/link';
 
 export default function Navigation(props) {
@@ -17,8 +17,18 @@ export default function Navigation(props) {
     width > 768 ? (
       <div className="flex flex-row items-center justify-between p-4 pt-8 mx-16 mb-4">
         <div className="flex flex-row items-center gap-x-8">
-          <Link href="/" className="text-xl cursor-pointer font-light duration-100 hover:scale-[101%]">Servers</Link>
-          <p className="text-xl cursor-pointer font-light duration-100 hover:scale-[101%]">User Management</p>
+          <div className="flex flex-row gap-x-2 items-center hover:scale-[101%]">
+            <FaServer/>
+            <Link href="/" className="text-xl cursor-pointer font-light duration-100">Servers</Link>
+          </div>
+          <div className="flex flex-row gap-x-2 items-center hover:scale-[101%]">
+            <FaUserCircle/>
+            <p className="text-xl cursor-pointer font-light duration-100">User Management</p>
+          </div>
+          <div className="flex flex-row gap-x-2 items-center hover:scale-[101%]">
+            <FaLock/>
+            <p className="text-xl cursor-pointer font-light duration-100">Authentication Stores</p>
+          </div>
         </div>
         {session && <p>Signed in as <span className="font-semibold">{session.user.name}</span></p>}
       </div>
@@ -29,9 +39,19 @@ export default function Navigation(props) {
           {session && <p>Signed in as <span className="font-semibold">{session.user.name}</span></p>}
         </div>
         {open && (
-          <div className="flex flex-col justify-center w-screen px-4 mb-4">
-            <Link href="/" className="text-xl cursor-pointer font-light duration-100 hover:scale-[101%]">Servers</Link>
-            <p className="text-xl cursor-pointer font-light duration-100 hover:scale-[101%]">User Management</p>
+          <div className="flex flex-col justify-center w-screen px-4 mb-4 gap-y-4">
+            <div className="flex flex-row gap-x-2 items-center hover:scale-[101%]">
+              <FaServer/>
+              <Link href="/" className="text-xl cursor-pointer font-light duration-100">Servers</Link>
+            </div>
+            <div className="flex flex-row gap-x-2 items-center hover:scale-[101%]">
+              <FaUserCircle/>
+              <p className="text-xl cursor-pointer font-light duration-100">User Management</p>
+            </div>
+            <div className="flex flex-row gap-x-2 items-center hover:scale-[101%]">
+              <FaLock/>
+              <p className="text-xl cursor-pointer font-light duration-100">Authentication Stores</p>
+            </div>
           </div>
         )}
       </div>
